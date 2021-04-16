668956f6a167fd8eea232d4b4a5ab19d1f971852
anteater-wsl <anteater1056@gmail.com>
2021-04-16 21:52:50 +0900

    channel controller unit test code 구체화

--- api/channel/channel.spec.js
+++ api/channel/channel.spec.js
@@ -26,7 +26,7 @@ const testData = {
 };
 
 describe('POST /channels', () => {
-    it('상태 코드 201을 반환한다.', (done) => {
+    it('상태 코드 201을 반환한다.', () => {
         request(app)
             .post('/channels')
             .set("Content-Type", "application/json")
@@ -34,23 +34,21 @@ describe('POST /channels', () => {
             .expect(201)
             .end((err, res) => {
                 if (err) throw err;
-                done();
             });
     });
 });
 
 describe('GET /channels', () => {
-    it('상태 코드 200을 반환한다. ', (done) => {
+    it('상태 코드 200을 반환한다. ', () => {
         request(app)
             .get('/channels')
             .expect(200)
             .end((err, res) => {
                 if (err) throw err;
-                done();
             });
     });
 
-    it('Channel 스키마에 부합한 데이터를 얻는다.', (done) => {
+    it('Channel 스키마에 부합한 데이터를 얻는다.', () => {
         request(app)
             .get('/channels')
             .expect(200)
@@ -67,33 +65,102 @@ describe('GET /channels', () => {
                     channel.dateCreated.should.be.a.String();
                     channel.channelOptions.should.be.an.Object().and.have.properties('playlist', 'backgroundURL');
                 });
-
-                done();
             });
     });
 });
 
 describe('GET /channels/:channelName', () => {
-    it('channelName에 해당하는 자원을 얻어 상태 코드 200을 반환한다.', (done) => {
-        request(app)
-            .get('/channels/' + testData.channelName)
-            .expect(200)
-            .end((err, res) => {
-                if (err) throw err;
-                res.body.should.have.properties('channelName', 'description', 'tags', 'favorited', 'dateCreated', 'channelOptions');
-                done();
-            });
+    describe('성공 시', () => {
+        it('상태 코드 200을 반환한다.', () => {
+            request(app)
+                .get('/channels/' + testData.channelName)
+                .expect(200)
+                .end((err, res) => {
+                    if (err) throw err;
+                });
+        });
+
+        it('channelName에 해당하는 자원을 얻는다.', () => {
+            request(app)
+                .get('/channels/' + testData.channelName)
+                .expect(200)
+                .end((err, res) => {
+                    if (err) throw err;
+                    should(res.body).have.properties('channelName', 'description', 'tags', 'favorited', 'dateCreated', 'channelOptions');
+                    should(res.body.channelName).equal(testData.channelName);
+                });
+        });
+    });
+
+    describe('실패 시', () => {
+        it('존재하지 않는 channelName을 요청 시 404 코드를 반환한다.', () => {
+            request(app)
+                .get('/channels/' + 'TESTCHANNELDOESNOTEXIST')
+                .expect(404)
+                .end((err, res) => {
+                    if (err) throw err;
+                });
+        });
     });
 })
 
+describe('PUT /channels/:channelName', () => {
+    describe('성공 시', () => {
+        const newDesc = "test입니다."
+        it('상태 코드 200를 반환한다.', () => {
+            request(app)
+                .put('/channels/' + testData.channelName)
+                .expect(200)
+                .set("Content-Type", "application/json")
+                .send( {description: newDesc} )
+                .end((err, res) => {
+                    if (err) throw err;
+                });
+        });
+
+        it('자원의 요청한 부분만 변경이 반영되었다.', () => {
+            request(app)
+                .get('/channels/' + testData.channelName)
+                .expect(200)
+                .end((err, res) => {
+                    if (err) throw err;
+                    
+                    should(res.body.channelName).be.equal(testData.channelName)
+                    should(res.body.description).be.equal(newDesc)
+                });
+        });
+    })
+});
+
 describe('DELETE /channels/:channelName', () => {
-    it('channelName에 해당하는 자원이 삭제되어 상태 코드 204를 반환한다.', (done) => {
-        request(app)
-            .delete('/channels/' + testData.channelName)
-            .expect(204)
-            .end((err, res) => {
-                if (err) throw err;
-                done();
-            });
+    describe('성공 시', () => {
+        it('상태 코드 204를 반환한다.', () => {
+            request(app)
+                .delete('/channels/' + testData.channelName)
+                .expect(204)
+                .end((err, res) => {
+                    if (err) throw err;
+                });
+        });
+
+        it('해당 채널이 삭제되어 요청 시 404 코드를 반환한다.', () => {
+            request(app)
+                .get('/channels/' + testData.channelName)
+                .expect(404)
+                .end((err, res) => {
+                    if (err) throw err;
+                });
+        });
+    });
+
+    describe('실패 시', () => {
+        it('존재하지 않는 channelName을 요청 시 404 코드를 반환한다.', () => {
+            request(app)
+                .delete('/channels/' + 'TESTCHANNELDOESNOTEXIST')
+                .expect(404)
+                .end((err, res) => {
+                    if (err) throw err;
+                });
+        });
     });
 });
